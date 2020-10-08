4e9e2d68c2c0730d8050540991fa7480ab11caf9
sulaiman408 <sulaiman.mahmood408@gmail.com>
2020-10-08 10:44:35 +0500

    master

---
+++ New Unity Project (2)/Assets/Scripts/Scriptable Objects/ItemPickUps_SO.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemTypeDefinitions { HEALTH, WEALTH, MANA, WEAPON, ARMOR, BUFF, EMPTY };
+public enum ItemArmorSubType { None, Head, Chest, Hands, Legs, Boots };
+
+[CreateAssetMenu(fileName = "NewItem", menuName = "Spawnable Item/New Pick-up", order = 1)]
+public class ItemPickUps_SO : ScriptableObject
+{
+    public string itemName = "NewItem";
+    public ItemTypeDefinitions itemType = ItemTypeDefinitions.HEALTH;
+    public ItemArmorSubType itemArmorSubType = ItemArmorSubType.None;
+    public int itemAmount = 0;
+    public int spawnChanceWeight = 0;
+
+    public Material itemMaterial = null;
+    public Sprite ItemIcon=null;
+    public Rigidbody itemSpawnObject = null;
+    public Rigidbody weaponSlotObject = null;
+
+    public bool isEquipped = false;
+    public bool isInteractable = false;
+    public bool isStorable = false;
+    public bool isUnique = false;
+    public bool isIndestructible = false;
+    public bool isQuestItem = false;
+    public bool isStackable = false;
+    public bool destroyOnUse = false;
+    public float itemWeight = 0f;
+}
